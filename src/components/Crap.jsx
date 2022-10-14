8fcebe451885c8bad3da06ea1bcfc56a62bc19af
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-14 16:53:38 +0800

    go back to GameTitle.jsx from MySavedGame.jsx

--- src/components/Crap.jsx
+++
@@ -1,3 +0,0 @@
-export function Crap(prop) {
-  return <h1>{prop.name}</h1>;
-}
--- src/pages/MySavedGame.jsx
+++ src/pages/MySavedGame.jsx
@@ -1,8 +1,7 @@
 import React, { useContext } from "react";
-import { Crap } from "../components/Crap";
 import { range } from "lodash-es";
 import { EventBusContext } from "../lib/GlobalVariable.js";
-import { evResumeSavedGame } from "../lib/Events.js";
+import { evBackToGameTitle, evResumeSavedGame } from "../lib/Events.js";
 
 function MySavedGame() {
   const eb = useContext(EventBusContext);
@@ -11,7 +10,10 @@ function MySavedGame() {
   }
   return (
     <>
-      <Crap name={"此处显示各历史对局"}></Crap>
+      <h1>此处显示各历史对局</h1>
+      <button onClick={() => eb.publish(evBackToGameTitle())}>
+        返回主菜单
+      </button>
       {range(1, 6).map((idx) => (
         <button key={idx} onClick={() => handleResumeGame(idx)}>
           从残局{idx}开始玩

e72d84a1f7ed56786016f803400f37c0f4f0ca02
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-15 22:03:59 +0800

    fetch my saved games from cloud

--- src/pages/MySavedGame.jsx
+++ src/pages/MySavedGame.jsx
@@ -1,13 +1,38 @@
-import React, { useContext } from "react";
-import { range } from "lodash-es";
+import React, { useContext, useEffect, useState } from "react";
 import { EventBusContext } from "../lib/GlobalVariable.js";
 import { evBackToGameTitle, evResumeSavedGame } from "../lib/Events.js";
+import axios from "axios";
+import { BarLoader } from "react-spinners";
+import { sleep } from "../lib/utility";
+import { css } from "@emotion/react";
+
+async function fetchSavedGames() {
+  for (let i = 0; i < 3; i++) {
+    try {
+      return (await axios.get("/saved_games/pABCDABCDABCD")).data;
+    } catch (e) {
+      await sleep(300);
+    }
+  }
+  return [];
+}
 
 function MySavedGame() {
   const eb = useContext(EventBusContext);
-  function handleResumeGame(game_index) {
-    eb.publish(evResumeSavedGame(game_index));
+  function handleResumeGame(match_token) {
+    eb.publish(evResumeSavedGame(match_token));
   }
+  const [savedGames, setSavedGames] = useState([]);
+  const [show, setSpinner] = useState(true);
+  const turnOffSpinner = () => setSpinner(false);
+
+  useEffect(() => {
+    fetchSavedGames().then((r) => {
+      turnOffSpinner();
+      setSavedGames(r);
+    });
+  }, []);
+
   return (
     <>
       <h1 className={"header"}>此处显示各历史对局</h1>
@@ -18,15 +43,30 @@ function MySavedGame() {
         >
           返回主菜单
         </div>
-        {range(1, 6).map((idx) => (
+        {show && (
           <div
-            className={"btn"}
-            key={idx}
-            onClick={() => handleResumeGame(idx)}
+            css={css`
+              display: flex;
+              align-items: center;
+              gap: 10px;
+            `}
           >
-            从残局{idx}开始玩
+            <span>加载中</span>
+            <BarLoader color={"#FFFFFF"} />
           </div>
-        ))}
+        )}
+        {savedGames.map((save) => {
+          const { idx, match_token } = save;
+          return (
+            <div
+              className={"btn"}
+              key={idx}
+              onClick={() => handleResumeGame(match_token)}
+            >
+              从残局{idx}开始玩
+            </div>
+          );
+        })}
       </main>
     </>
   );

455bcc6f0ee69c2f5164b988999d848b03919fac
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-15 19:21:31 +0800

    add msw to mock http requests and responses

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import App from "./App";
 import EventBus from "./lib/EventBus.js";
 import { EventBusContext } from "./lib/GlobalVariable.js";
 
+if (import.meta.env.DEV) {
+  import("./mocks/browser").then((module) => {
+    module.default.start();
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <EventBusContext.Provider value={EventBus}>
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,36 @@
+import { rest } from "msw";
+import { last, repeat } from "lodash-es";
+
+export const handlers = [
+  rest.post("/register", async (req, res, ctx) => {
+    if (
+      localStorage.getItem("kingz-nickName") !== null &&
+      localStorage.getItem("kingz-secret") !== null
+    ) {
+      return res(ctx.status(409), ctx.text("user already exists"));
+    }
+
+    const text = await req.text();
+    const [nk_pair, secret_pair] = text.split("&");
+    const [nnk, nickName] = nk_pair?.split("=");
+    const [sk, secret] = secret_pair?.split("=");
+    if (
+      nnk !== "nickName" ||
+      sk !== "secret" ||
+      sk === undefined ||
+      nnk === undefined
+    ) {
+      return res(ctx.status(400));
+    } else {
+      const n = decodeURIComponent(nickName);
+      const s = decodeURIComponent(secret);
+      localStorage.setItem("kingz-nickName", n);
+      localStorage.setItem("kingz-secret", s);
+      return res(ctx.status(200), ctx.text(`p${repeat("Y", 11)}`));
+    }
+  }),
+  rest.post("/match/:player_id", async (req, res, ctx) => {
+    const player_id = last(req.url.pathname.split("/"));
+    return res(ctx.status(200), ctx.text(`GridToken${player_id}`));
+  }),
+];

2e2e82fe38fb55aad34b7d11822731c2c7e8131a
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-23 20:56:36 +0800

    remove unused page symbol

--- src/lib/PageSymbol.ts
+++ src/lib/PageSymbol.ts
@@ -4,4 +4,3 @@ export const pgGameTitle = "游戏主屏幕";
 export const pgGamePage = "游戏棋盘";
 export const pgWaitingInQueue = "匹配等待中";
 export const pgRemotePlayerWentOffline = "你的对手离开了对局";
-export const pgGameOver = "GameOver";

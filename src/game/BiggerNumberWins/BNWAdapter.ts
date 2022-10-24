04401d9577e04e9ea7c6fd8b4e2ebf83fb0d2de7
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-24 11:26:45 +0800

    new game adapter

---
+++ src/game/BiggerNumberWins/BNWAdapter.ts
@@ -0,0 +1,48 @@
+import IGameAgent from "../IGameAgent";
+import {
+  evLocalMove,
+  evUpdateGameState,
+  evInitGameState,
+  evGameOver,
+  evPushLocalGameStateToCloud,
+} from "../../lib/Events";
+
+export interface BNWGameState {
+  remote_moved: boolean;
+  remote_number: number | null;
+  my_number: number;
+}
+
+export default class BNWAdapter implements IGameAgent {
+  event_bus: any;
+  game: BNWGameState;
+
+  private should_terminate(): boolean {
+    return this.game.remote_number !== null;
+  }
+
+  attach_event_bus(event_bus: any, init_game_state: any) {
+    this.event_bus = event_bus;
+    this.event_bus.subscribe(evLocalMove(), this.handleLocalMove.bind(this));
+  }
+
+  handleCloudUpdate(game_state: BNWGameState) {
+    if (this.game === null) {
+      this.game = game_state;
+      this.event_bus.publish(evInitGameState(this.game));
+    }
+    this.game.remote_moved = game_state.remote_moved;
+  }
+
+  handleLocalMove(move: number) {
+    this.event_bus.publish(evPushLocalGameStateToCloud(this.game));
+    this.event_bus.publish(evUpdateGameState(this.game));
+    if (this.should_terminate()) {
+      this.event_bus.publish(
+        evGameOver(
+          this.game.remote_number < this.game.my_number ? "local" : "remote"
+        )
+      );
+    }
+  }
+}
